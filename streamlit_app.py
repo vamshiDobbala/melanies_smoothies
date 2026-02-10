9c50ce81fdec706ad4c1d8897f346f8c501e68d3
Vamsi Dobbala <vamshidammalapati@gmail.com>
2026-02-10 18:13:40 +0530

    rearranged dataframes

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     # st.write(ingredients_string)    
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, NAME_ON_ORDER)
             values ('""" + ingredients_string +"""','"""+name_on_order+ """')"""
@@ -33,6 +35,3 @@ if ingredients_list:
     if time_to_inert:
         session.sql(my_insert_stmt).collect()
         st.success('Your smoothie is ordered', icon= "✅" )
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
