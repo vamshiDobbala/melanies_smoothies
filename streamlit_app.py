4b712530cfbb5a3340380330b3a30087be22875f
Vamsi Dobbala <85015104+vamshiDobbala@users.noreply.github.com>
2026-02-10 15:22:35 +0530

    Update Snowflake connection and imports in app
    Connected changes needed because of moving SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
 
 # Write directly to the app
 st.title("🥤Customize Your Smoothie🥤")
@@ -11,10 +12,8 @@ st.write(
 )
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your smoothie will be:", name_on_order)
-
-from snowflake.snowpark.functions import col
-
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect('Choose up to 5 ingredients: ', my_dataframe, max_selections = 5)
